2695b3c65b9a4c6954fea81b5e2a5c48b04d38a4
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-11-17 04:54:53 +0100

    Základní funkčnost.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,8 +2,8 @@
 
 namespace App\Presenters;
 
-use Nette,
-	App\Model;
+use Nette\Application\UI\Form;
+use Nette\Utils\Strings;
 
 
 /**
@@ -12,9 +12,88 @@ use Nette,
 class HomepagePresenter extends BasePresenter
 {
 
-	public function renderDefault()
+	public function renderDefault($hash = NULL)
 	{
-		$this->template->anyVariable = 'any value';
+		if ($hash)
+		{
+			$latinText = $this->session->getSection('runes')->$hash;
+			$this->template->hasData = TRUE;
+			$this->template->latinText = $latinText;
+			$this->template->runeText = $this->latin2runes(strtolower(Strings::toAscii($latinText)));
+		}
+		else
+		{
+			$this->template->hasData = FALSE;
+		}
+	}
+	
+	protected function createComponentInputForm()
+	{
+		$form = new Form;
+		$form->addTextArea('text', "Text")
+				->addRule(Form::FILLED, "Zadejte text, který má být přeložen.");
+		$form->addSubmit('ok', "Přeložit");
+		$form->onSuccess[] = $this->formSuccess;
+		
+		return $form;
+	}
+	
+	public function formSuccess(Form $form)
+	{
+		$latinText = $form->values->text;
+		$hash = hash('md5', $latinText);
+		$this->session->getSection('runes')->$hash = $latinText;
+		$this->redirect('this', $hash);
+		
+	}
+	
+	protected function latin2runes($latinText)
+	{
+		$runeTable = [
+			'a' => "ᚫ",
+			'b' => "ᛒ",
+			'c' => "ᚲ",
+			'd' => "ᛞ",
+			'e' => "ᛖ",
+			'f' => "ᚠ",
+			'g' => "ᚷ",
+			'h' => "ᚺ",
+			'i' => "ᛁ",
+			'j' => "ᛃ",
+			'k' => "ᚲ",
+			'l' => "ᛚ",
+			'm' => "ᛗ",
+			'n' => "ᚾ",
+			'o' => "ᛟ",
+			'p' => "ᛈ",
+			'q' => "ᛜ", # Ng
+			'r' => "ᚱ",
+			's' => "ᛋ",
+			't' => "ᛏ",
+			'u' => "ᚢ",
+			'v' => "ᚹ",
+			'w' => "ᚹ",
+			'x' => "ᚦ", # Th
+			'y' => "ᛇ",
+			'z' => "ᛉ",
+			' ' => " ",
+//			',' => "᛬",
+			'.' => "᛫"
+		];
+		
+		$runeText = "";
+		
+		foreach(str_split($latinText) as $char) {
+			if(ord($char) === 10) { # zachováme odřádkování
+				$runeText .= $char;
+			}
+			else {
+				if (isset($runeTable[$char]))
+					$runeText .= $runeTable[$char];
+			}
+		}
+		
+		return $runeText;
 	}
 
 }
